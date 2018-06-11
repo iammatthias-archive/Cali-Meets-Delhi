3eac3e2a86883550a181e3d8f3ad3137c150674d
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-06-10 18:57:21 -0700

    Update

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,12 +1,5 @@
 const theme = {
   colors: {
-    paleBlue: '#E9EFE9', // Alice Blue
-    yellow: '#F2A541', // BRIGHT YELLOW (CRAYOLA)
-    yellowAlt: '#F2A541', // MAXIMUM YELLOW RED
-    green: '#E0E5FF', // Teal Deer
-    red: '#FF8A42', // PERMANENT GERANIUM LAKE
-    redAlt: '#D11B1B', // CORAL RED
-
     pureWhite: '#ffffff',
     black: '#223843', // JAPANESE INDIGO
     bg1: '#DBD3D8',
