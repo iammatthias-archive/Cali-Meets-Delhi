2bc4564bfc97512d3b7ac1ef1e6720e107bd134a
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-10-12 08:49:17 -0700

    Yarn

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -145,7 +145,7 @@ class ContactForm extends React.Component {
   }
 
   handleSubmit = event => {
-    fetch('/#contact?no-cache=1', {
+    fetch('/?no-cache=1', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({ 'form-name': 'contact', ...this.state }),
