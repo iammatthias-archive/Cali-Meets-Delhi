94fa315d076cdc3f57c8ed050339a109408f3318
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-10-16 17:49:32 -0700

    Remove comments

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -99,21 +99,12 @@ class Menu extends React.Component {
       menuOpen: false,
     }
   }
-
-  // This keeps your state in sync with the opening/closing of the menu
-  // via the default means, e.g. clicking the X, pressing the ESC key etc.
   handleStateChange(state) {
     this.setState({ menuOpen: state.isOpen })
   }
-
-  // This can be used to close the menu, e.g. when a user clicks a menu item
   closeMenu() {
     this.setState({ menuOpen: false })
   }
-
-  // This can be used to toggle the menu, e.g. when using a custom icon
-  // Tip: You probably want to hide either/both default icons if using a custom icon
-  // See https://github.com/negomi/react-burger-menu#custom-icons
   toggleMenu() {
     this.setState({ menuOpen: !this.state.menuOpen })
   }
